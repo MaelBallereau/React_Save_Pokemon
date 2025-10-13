1f64ab07f5866b21c743ca11ec4698f911d9879c
MaelBallereau <ballereau.mael@educ-valadon-limoges.fr>
2025-10-13 16:53:49 +0200

    feat: :sparkles: CharactereCard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
+import "../../public/styles/HomePage/styles.scss";
+import CharacterCard from "../components/HomePage/CharacterCard";
+
 const HomePage = () => {
   return (
     <>
       <div className="title">
-        <img src="public/img/title_pokemon.png" alt="Pokemon title" />
-        <h1>Save Pokemon</h1>
-        <h2>Choisir votre Pokemon</h2>
+        <img
+          className="title-img"
+          src="public/img/title_pokemon.png"
+          alt="Pokemon title"
+        />
+        <h2 className="title-text">Choisir votre Pokemon</h2>
+      </div>
+      <div className="content">
+        <CharacterCard avatar="/public/img/bravhilde-briseflamme.png" firstname="Bravhilde" lastname="Briseflamme" race="Guerrière Humaine" health="180" energy="0" attack="18" defense="30" money="0" />
       </div>
     </>
   );
