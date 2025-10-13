bcddf4de05a41e5528f040e5ca01c956fa939b84
MaelBallereau <ballereau.mael@educ-valadon-limoges.fr>
2025-10-13 16:23:35 +0200

    feat: :sparkles: Pokemon Logo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="title">
+        <img src="public/img/title_pokemon.png" alt="Pokemon title" />
+        <h1>Save Pokemon</h1>
+        <h2>Choisir votre Pokemon</h2>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
