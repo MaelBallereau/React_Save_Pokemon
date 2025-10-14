e9f3ab07ab33a0584b88bafe192a9d791ec4de55
jordandbs <desbordes.jordan@educ-valadon-limoges.fr>
2025-10-14 12:01:43 +0200

    feat: :sparkles: update and inventorylist with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 
 import HomePage from "./pages/HomePage";
+import InventoryPage from "./pages/InventoryPage";
 import FailurePage from "./pages/FailurePage"; 
 
 function App() {
@@ -8,12 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/inventaire" element={<HomePage />} />
-        <Route path="/quete" element={<HomePage />} />
-        <Route path="/combat" element={<HomePage />} />
-        <Route path="/victoire" element={<HomePage />} />
-        <Route path="/echec" element={<FailurePage />} />
-        <Route path="/fin" element={<HomePage />} />
+        <Route path="/inventaire" element={<InventoryPage />} />
+        <Route path="/quete" element={<FailurePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/InventoryPage/InventoryList.jsx
@@ -0,0 +1,3 @@
+export default function InventoryList() {
+  return <div className="inventory-list"></div>;
+}
