d5d13d3fb04d254ffe12b5eed9ab57d4ea92be98
MaelBallereau <ballereau.mael@educ-valadon-limoges.fr>
2025-10-15 08:54:58 +0200

    feat: :sparkles: updated component button

--- src/components/InventoryPage/BackButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function BackButton() {
-
-  return (
-    <button className="back-button" onClick={() => window.location.href = "/inventaire"}>
-      ← Retour aux quêtes
-    </button>
-  );
-}
--- src/pages/QuestPage.jsx
+++ src/pages/QuestPage.jsx
@@ -1,26 +1,35 @@
 import "../../public/styles/QuestPage/styles.scss";
 import Card from "../components/QuestPage/Card.jsx";
-import BoutonInventaire from "../components/QuestPage/BoutonInventaire.jsx";
-import BoutonCombat from "../components/QuestPage/BoutonCombat.jsx";
 import Quest from "../../public/data/quests.json";
+import Button from "../components/GlobalComponents/Button.jsx";
 
 const QuestPage = () => {
   const quests = JSON.parse(localStorage.getItem("quests")) || Quest;
   const id = JSON.parse(localStorage.getItem("id")) || 0;
+  const handleCombat = () => {
+    window.location.href = "/combat";
+  };
+  const handleInventaire = () => {
+    window.location.href = "/inventaire";
+  };
   return (
     <>
       <div className="titles">
-          <h1> Quêtes </h1>
+        <h1> Quêtes </h1>
       </div>
       <div className="content-QuestPage">
-          <Card title={quests[id]?.title} description={quests[id]?.description} objective={quests[id]?.objective} />
-            <div className="btn">
-                <BoutonInventaire />
-            </div>
-            <div className="btn">
-                <BoutonCombat />
-            </div>
-      </div>  
+        <Card
+          title={quests[id]?.title}
+          description={quests[id]?.description}
+          objective={quests[id]?.objective}
+        />
+        <div className="btn">
+          <Button onClick={handleInventaire} text="Inventaire" />
+        </div>
+        <div className="btn">
+          <Button onClick={handleCombat} text="Combat" />
+        </div>
+      </div>
     </>
   );
 };
