eaca213aa0b6e5a785d6d90406b76978923b36f8
jordandbs <desbordes.jordan@educ-valadon-limoges.fr>
2025-10-15 11:45:12 +0200

    feat: :sparkles: update data to src

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -5,12 +5,13 @@ import "../../public/styles/HomePage/styles.scss";
 
 import CharacterCard from "../components/HomePage/CharacterCard.jsx";
 import InventoryList from "../components/InventoryPage/InventoryList.jsx";
-import ShopPanel from "../components/InventoryPage/ShopPanel.jsx";
+// import ShopPanel from "../components/InventoryPage/ShopPanel.jsx";
+import DynamicBar from "../components/GlobalComponents/DynamicBar.jsx";
 import Button from "../components/GlobalComponents/Button.jsx";
 
 export default function InventoryPage() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
-  
+
   useEffect(() => {
     const storedData = localStorage.getItem("selectedcharacter");
     if (storedData) {
@@ -23,34 +24,49 @@ export default function InventoryPage() {
   };
 
   return (
-    <>
+    <section className="inventory-page">
       <div className="titles">
-        <h1>Mon Inventaire</h1>
+        <h1>Inventaire</h1>
       </div>
+
+      {selectedCharacter && (
+        <div className="money-display">
+          <img src="/img/money.png" alt="money" className="money-icon" /> 
+          <span>{selectedCharacter.fortune}</span>
+        </div>
+      )}
 
       <div className="pannel">
         {selectedCharacter && (
-          <CharacterCard className="CharacterCard" avatar={ `/img/${selectedCharacter.picture}`}
-           firstname={selectedCharacter.name} 
-           race={selectedCharacter.class} 
-           health={selectedCharacter.health} 
-           energy={selectedCharacter.mana} 
-           attack={selectedCharacter.damage} 
-           defense={selectedCharacter.defense} 
-           money={selectedCharacter.fortune} 
-           spells={selectedCharacter.spell? [selectedCharacter.spell] : []} 
-           onClick={selectedCharacter.onClick} 
-           isSelected={selectedCharacter.isSelected} />
+          <div className="character-card-container">
+            <div className="character-card-wrapper">
+              <CharacterCard
+                className="CharacterCard"
+                avatar={`/img/${selectedCharacter.picture}`}
+                firstname={selectedCharacter.name}
+                race={selectedCharacter.class}
+                health={selectedCharacter.health}
+                energy={
+                  <DynamicBar
+                    type="energy"
+                    value={selectedCharacter.mana}
+                    max={100}
+                  />
+                }
+                attack={selectedCharacter.damage}
+                defense={selectedCharacter.defense}
+                spells={selectedCharacter.spell ? [selectedCharacter.spell] : []}
+                isSelected={selectedCharacter.isSelected}
+              />
+            </div>
+          </div>
         )}
+
         <InventoryList className="inventory-list" />
-        <ShopPanel className="shop-panel" />
+        {/* <ShopPanel className="shop-panel" /> */}
       </div>
 
       <Button onClick={handleBack} text="Retour" />
-    </>
+    </section>
   );
 }
-
-  
-  
- 
