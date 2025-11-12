24aacdc909b3841bafac0302e1a106e5ee42c181
David24330 <lefouduson777@gmail.com>
2025-11-12 10:04:01 +0100

    feat: :sparkles: Add gold for quests

--- src/pages/VictoryPage/VictoryPage.jsx
+++ src/pages/VictoryPage/VictoryPage.jsx
@@ -1,10 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import { useGameContext } from "../../Context/GameContext.jsx";
+import { useEffect, useState } from "react";
+import Quest from "../../data/quests.json";
 import "./styles.scss";
 
 export default function VictoryPage() {
   const navigate = useNavigate();
+  const { setSelectedCharacter, selectedCharacter } = useGameContext();
+  const [showGoldPopup, setShowGoldPopup] = useState(false);
+  const idQuest = localStorage.getItem("idQuest") - 1;
+  const currentQuest = Quest.find((q) => q.id === idQuest);
+  const rewardGold = currentQuest?.reward?.gold;
 
-  const idQuest = localStorage.getItem("idQuest")-1;
+  useEffect(() => {
+    if (rewardGold) {
+      const storedCharacter = JSON.parse(localStorage.getItem("selectedcharacter"));
+
+      const updatedCharacter = {
+        ...storedCharacter,
+        fortune: (storedCharacter.fortune || 0) + rewardGold,
+      };
+
+      setSelectedCharacter(updatedCharacter);
+
+      setShowGoldPopup(true);
+
+      const timer = setTimeout(() => setShowGoldPopup(false), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, []);
 
   const handleNextQuest = () => {
     navigate("/quete");
@@ -12,6 +36,12 @@ export default function VictoryPage() {
 
   return (
     <div className="victory-page">
+      {showGoldPopup && (
+        <div className="gold-popup">
+          <img src="/img/money.png" alt="gold" />
+          <span>+{rewardGold} Gold !</span>
+        </div>
+      )}
       <div className="card">
         <h1 className="pokemon-title">✨ Victoire ! ✨</h1>
         <p className="subtitle">
